246734c400aebcef1ceb91eaf5ad61749e2b1b97
ren burnett <elcreature@live.com>
2017-07-19 18:39:07 -0700

    installed express etc

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,15 +1,23 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE: Initialize your project using NPM to create and populate a package.json file
+//took 5min ran NPM init in the terminal
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
+//took 5min ran npm init in the terminal
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// DONE?????: Include all of the static resources as an argument to app.use()
+const express =
+  require('express');
+
+const app = express();
+
+app.use(express.static('/public/'));
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
